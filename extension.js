54ed7c8f7d8159a6cea88cf3d3c3d5e350c5b958
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2025-12-05 17:12:49 +0600

    Alhamdulillah, stable

--- extension.js
+++ extension.js
@@ -93,31 +93,33 @@ export default class NotificationThemeExtension extends Extension {
 
     // Connect to icon-size-changed to force our size
     this._iconSizeSignal = dash.connect('icon-size-changed', () => {
+      journal(`Icon size change`);
       if (dash.iconSize !== 112) {
+        journal(`Icon size not 112`);
         dash.iconSize = 112;
 
-        // Manually update all icon sizes
-        const iconChildren = dash._box?.get_children().filter(actor => {
-          return actor.child?._delegate?.icon;
-        }) || [];
+        // // Manually update all icon sizes
+        // const iconChildren = dash._box?.get_children().filter(actor => {
+        //   return actor.child?._delegate?.icon;
+        // }) || [];
 
-        iconChildren.forEach(actor => {
-          const icon = actor.child?._delegate?.icon;
-          if (icon) {
-            icon.setIconSize(112);
-          }
-        });
+        // iconChildren.forEach(actor => {
+        //   const icon = actor.child?._delegate?.icon;
+        //   if (icon) {
+        //     icon.setIconSize(112);
+        //   }
+        // });
 
-        // Update show apps icon
-        if (dash._showAppsIcon?.icon) {
-          dash._showAppsIcon.icon.setIconSize(112);
-        }
+        // // Update show apps icon
+        // if (dash._showAppsIcon?.icon) {
+        //   dash._showAppsIcon.icon.setIconSize(112);
+        // }
       }
     });
 
     // Set initial size
     dash.iconSize = 112;
-    dash.emit('icon-size-changed');
+    // dash.emit('icon-size-changed');
   }
 
   disable() {
